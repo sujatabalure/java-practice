9bbda318efe2d2aab03ba80102f6aef48138f049
sujatabalure <168956293+sujatabalure@users.noreply.github.com>
2024-05-07 21:40:16 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,35 @@
- class Arraynum
+class Isprime
 {
-    int[] array(int a)
-    {
-        int len = 0;
-        int acopy =a;
-      
-        while(a != 0)
+    boolean prime(int num)
+    {  
+        int i=0;          
+        for(i=2; num%i!=0; i++)
         {
-           a= a/10;
-           len++;
-           
-        }
-        
-        int[] Arr = new int[len];
-        
-        for(int i=0; i<len ;i++)
-        {
-           Arr[i] =acopy %10;
-           acopy = acopy/10;
-        }
-        return Arr;
-    } 
+	     }
+	     if(i == num)
+	     {
+	        return true;
+    	  } 
+	      else
+	     {
+	       return false ;
+	      }   
+     }
     
-        
 }
 public class Main
 {
-	public static void main(String[] args) {
-	    int a =12345;
-	    Arraynum obj1 = new Arraynum();
-	    int[] obj2 = obj1.array(a);
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("enter a number");
+	    int num = sc.nextInt();
+		    
+	    Isprime obj1 = new Isprime();
+	    boolean obj2 = obj1.prime(num);
 	    
-	    for (int i : obj2)
-	    {
-	        
-	         System.out.println("a integer array is " + i);
+	    System.out.println( num + " is a prime number " + obj2 );
 	         
-	    }
-	   
+
 	}
 }
