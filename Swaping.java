8c46e55f39c44d659534b4663a2677244605009b
Balure <s.tanaji@accenture.com>
2024-05-11 20:05:00 +0530

    swaping number program

---
+++ Swaping.java
@@ -0,0 +1,13 @@
+public class Swaping {
+    public static void main(String[] args) {
+        int num1 =20;
+        int num2 =30;
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("After swapping num1 value is "+ num1 + " and num2 value is " + num2);
+
+    }
+    
+}
